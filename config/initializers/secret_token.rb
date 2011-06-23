efac557708f2a665843932ecfbacd302b4295ede
Paulo Ragonha <paulo@pirelenito.org>
2011-06-23 19:12:19 -0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Makeabandtome::Application.config.secret_token = '3da35e7de8691a8fce66871469e9faf22d312c2e53546a04235de19c5211eec82715d38d645c5a796302796f8f67bfecee631ad8503d2be7c0084919997056c3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Makeabandtome::Application.config.session_store :cookie_store, :key => '_makeabandtome_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Makeabandtome::Application.config.session_store :active_record_store
